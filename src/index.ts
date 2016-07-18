069635d2fe0c6e71a9c1a1e7d9891ac289c6dbdf
Bryan Hughes <brhugh@microsoft.com>
2016-07-18 16:34:19 -0700

    Reorganized code into src/ folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
-import { init } from './root.ts';
+import { init } from './root';
 const app = express();
 
 init(app);
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('TypeScript example demo listening on port 3000!');
 });
